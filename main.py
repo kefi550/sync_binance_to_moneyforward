c9c134b3de455e745e5ef936f9497f29f6452c5e
kefi550 <ytiryrstd550@gmail.com>
2024-11-24 21:52:00 +0900

    MoneyforwardScraperを使う

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import os
 from binance_summary import get_latest_assets_jpy
-from moneyforward_selenium import update_mf_account
+from moneyforward_selenium import MoneyForwardScraper
+
+MONEYFORWARD_USER = os.environ["MONEYFORWARD_USER"]
+MONEYFORWARD_PASSWORD = os.environ["MONEYFORWARD_PASSWORD"]
 
 latest_asset_eth_jpy = get_latest_assets_jpy('ETH')
 if not latest_asset_eth_jpy:
     print('no latest asset')
     exit()
-update_mf_account("Binance", latest_asset_eth_jpy)
+
+mf = MoneyForwardScraper(MONEYFORWARD_USER, MONEYFORWARD_PASSWORD)
+mf.change_mf_group('グループ選択なし')
+mf.update_account_amount("Binance", latest_asset_eth_jpy)
